786bed92501962d47b32dcd60622db62b7781f7d
meepoorkeepo <DJFAYCEL06@GMAIL.COM>
2025-11-29 22:48:06 +0100

    update index.js to new pathing system new

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5001
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
-    app.get('/(.*)',(req,res)=>{
+    app.get('/(.*)/',(req,res)=>{
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
     })
 }
@@ -41,3 +41,4 @@ server.listen(PORT,()=>{
     
 
 })
+
